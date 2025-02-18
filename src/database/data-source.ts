a42bb34f6e87c7cd33bac5ce3c25434a04305593
Marco Antônio Meira de Lima <88397083+Marcokbc@users.noreply.github.com>
2025-02-17 23:38:09 -0300

    Ajuste no swagger

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -14,7 +14,7 @@ import User from "../app/entities/User";
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST,
-  port: Number(process.env.APPLICATION_PORT),
+  port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
